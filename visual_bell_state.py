0d225e901e852605f7fc570c9c3ac24e90121756
anonymouspython96 <emilianmail96@gmail.com>
2026-08-16 14:08:36 +0200

    Created visual version of the bell state with graphic thanks to matplotlib

---
+++ visual_bell_state.py
@@ -0,0 +1,20 @@
+#Visual bell state
+from qiskit import QuantumCircuit
+from qiskit.primitives import StatevectorSampler
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+dog_circuit = QuantumCircuit(2)
+dog_circuit.h(0)
+dog_circuit.cx(0, 1)
+dog_circuit.measure_all()
+
+universe = StatevectorSampler()
+result = universe.run([dog_circuit], shots=1084).result()
+print(result[0].data.meas.get_counts())
+
+#matplotlib magic
+counts = result[0].data.meas.get_counts()
+figure = plot_histogram(counts)
+
+plt.show()
